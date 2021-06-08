267881b11307ee89566fd6c6da6bcc1b5c74dec4
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2021-06-07 21:49:09 -0300

    Proyecto Finzalido

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -46,7 +46,9 @@ const Contacto = () => {
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3273.6417078573936!2d-58.04922008419892!3d-34.865229078889975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2df6860639995%3A0xf347e5212e3e781c!2sAires%20de%20City%20Bell%20Geriatrico!5e0!3m2!1ses-419!2sar!4v1623038560680!5m2!1ses-419!2sar"
               className="desktop-map__iframe"
-            ></iframe>
+              title = "Mapa-lugar"
+            >
+            </iframe>
           </div>
         </section>
       </Layout>
